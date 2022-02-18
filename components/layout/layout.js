631f456cbd3c5a94bab00827a6fcc722121de2ae
Papop Lekhapanyaporn <75293379+Retaehc-pop@users.noreply.github.com>
2022-02-18 22:38:15 +0700

    commit change ham

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -6,7 +6,8 @@ import { useState, useEffect } from 'react'
 import Link from 'next/link';
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; 
-import { faAdjust, faEnvelope, faLanguage, faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faAdjust, faBars, faEnvelope, faLanguage, faPhone} from "@fortawesome/free-solid-svg-icons";
+import { faXmarkCircle } from "@fortawesome/free-regular-svg-icons";
 import { faInstagram, faGithub, faStackOverflow,faDiscord, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { useUser } from '../../firebase/useUser';
 
@@ -24,16 +25,15 @@ function ActiveLink({ children, href}){
 
 export const Layout = ({children}) =>{
 	const { user, logout } = useUser()
-
+	const [isOpen,setIsOpen] = useState(false)
+	const [darkTheme, setDarkTheme] = useState(undefined);
+	let router = useRouter()
+	let { t } = useTranslation()
 	const date = new Date("December 20,2021");
 	const fullYear = date.getFullYear();
-	let router = useRouter()
-	const [darkTheme, setDarkTheme] = useState(undefined);
-	let { t } = useTranslation()
 	const storeUserSetPreference = (pref) => {
 		localStorage.setItem("theme",pref);
 	}
-
 	const getMediaQueryPreference = () =>{
 		const mediaQuery = "(prefers-color-scheme: dark)";
 		const mql = window.matchMedia(mediaQuery);
@@ -64,18 +64,21 @@ export const Layout = ({children}) =>{
 	}
 	}, [darkTheme]);
 
-
+	
 	return (
 	<div>
 		<nav className={styles.nav}>
-			<Link children="Home"  href="/" passHref>
-				<img src="/favicon.ico" width={50} height={50}/>
-			</Link>
-			<Link href="/about" passHref><a>{t("about:title")}</a></Link>
-			<Link href="/projects" passHref><a>{t("projects:title")}</a></Link>
-			<Link children="contact"  href="/contact" passHref><a>{t("common:contact")}</a></Link>
-			<Link href={router.asPath} locale={router.locales[(router.locales.indexOf(router.locale)+1)%router.locales.length]} passHref><a><FontAwesomeIcon icon={faLanguage} size='2x'/></a></Link>
-			<h3 className={styles.ico}><FontAwesomeIcon onClick={() => setDarkTheme(!darkTheme)} icon={faAdjust} size='2x'></FontAwesomeIcon></h3>
+				<h2 onClick={()=>setIsOpen(!isOpen)} ><FontAwesomeIcon icon={isOpen ? faXmarkCircle:faBars} size='2x'/></h2>
+				<div className={isOpen? "":styles.hidden}>
+					<Link children="Home"  href="/" passHref>
+						<img src="/favicon.ico" width={50} height={50}/>
+					</Link>
+					<Link href="/about" passHref><a>{t("about:title")}</a></Link>
+					<Link href="/projects" passHref><a>{t("projects:title")}</a></Link>
+					<Link children="contact"  href="/contact" passHref><a>{t("common:contact")}</a></Link>
+					<Link href={router.asPath} locale={router.locales[(router.locales.indexOf(router.locale)+1)%router.locales.length]} passHref><a><FontAwesomeIcon icon={faLanguage} size='2x'/></a></Link>
+					<h3 className={styles.ico}><FontAwesomeIcon onClick={() => setDarkTheme(!darkTheme)} icon={faAdjust} size='2x'/></h3>
+				</div>
 		</nav>
 		{children}
 		<footer className={styles.footer}>
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.scss'
 import { useRouter } from 'next/router'
 import { useState,useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBirthdayCake, faSearch,faCamera, faCode,faCog, faEnvelope, faGlobeAsia, faGraduationCap, faLanguage, faMapMarkedAlt, faPhone, faUserAstronaut} from '@fortawesome/free-solid-svg-icons'
+import { faBirthdayCake, faC, faCamera, faCode,faCog, faEnvelope, faGlobeAsia, faGraduationCap, faLanguage, faMapMarkedAlt, faPhone, faUserAstronaut, faCodeBranch, faKeyboard, faSatellite, faRocket, faMicrochip, faSatelliteDish, faAtom} from '@fortawesome/free-solid-svg-icons'
 
 import AwsSliderStyle from "react-awesome-slider/dist/custom-animations/fall-animation.css"
 import AwesomeSlider from 'react-awesome-slider';
@@ -22,6 +22,7 @@ import { collection, query, where, doc, getDoc, getDocs, arrayRemove } from "fir
 
 import useTranslation from "next-translate/useTranslation"
 import { Layout } from '../components/layout/layout'
+import { fa500px, faDocker, faGit, faGitAlt, faGithub, faJs, faPython } from '@fortawesome/free-brands-svg-icons'
 
 export const getStaticProps = async () => {
 	const allProject = [];
@@ -46,7 +47,6 @@ export default function Home({ projects }) {
     function parallax(e){
       this.querySelectorAll(".layer").forEach(layer => {
         const speed = layer.getAttribute('data-speed')
-        console.log(speed)
         const x = (window.innerWidth  - e.pageX*speed)/100
         const y = (window.innerHeight - e.pageY*speed)/100
 
@@ -69,16 +69,29 @@ export default function Home({ projects }) {
       <Layout>
       <main className={styles.main}>
         <section className={styles.startup}>
-          <img src="/img/BG.JPG" style={{position:"absolute",height:"100%",width:"100vw",filter:"blur(0.5rem)", WebkitFilter:"blur(0.5rem)"}}/>
-          <img src="/img/anti.png" data-speed="3" className="layer" style={{height:"50vmin", zIndex:"10"}} />
-         
+          <img src="/img/BG.JPG" />
+          {/* <img src="/img/anti.png" data-speed="3" className="layer" style={{height:"50vmin", zIndex:"10"}} /> */}
+          <h1 data-speed="-1" className="layer" >Retaehc</h1>
+          <p data-speed="2" className="layer" style={{display:"flex",top:"10%",left:"40%"}}><FontAwesomeIcon icon={faKeyboard}/></p>
+          <p data-speed="1" className="layer" style={{display:"flex",top:"10%",left:"80%"}}><FontAwesomeIcon icon={faSatelliteDish}/></p>
+          <p data-speed="1" className="layer" style={{display:"flex",top:"15%",left:"60%"}}><FontAwesomeIcon icon={faAtom}/></p>
+          <p data-speed="2" className="layer" style={{display:"flex",top:"15%",left:"15%"}}><FontAwesomeIcon icon={faPython}/></p>
+          <p data-speed="2" className="layer" style={{display:"flex",top:"45%",left:"80%"}}><FontAwesomeIcon icon={faRocket}/></p>
+          <p data-speed="8" className="layer" style={{display:"flex",top:"50%",left:"20%"}}><FontAwesomeIcon icon={faGitAlt}/></p>
+          <p data-speed="3" className="layer" style={{display:"flex",top:"60%",left:"40%"}}><FontAwesomeIcon icon={faCamera}/></p>
+          <p data-speed="6" className="layer" style={{display:"flex",top:"75%",left:"90%"}}><FontAwesomeIcon icon={faCode}/></p>
+          <p data-speed="7" className="layer" style={{display:"flex",top:"70%",left:"65%"}}><FontAwesomeIcon icon={faGithub}/></p>
+          <p data-speed="2" className="layer" style={{display:"flex",top:"80%",left:"10%"}}><FontAwesomeIcon icon={faCodeBranch}/></p>
+          <p data-speed="3" className="layer" style={{display:"flex",top:"80%",left:"50%"}}><FontAwesomeIcon icon={faJs}/></p>
+          <p data-speed="4" className="layer" style={{display:"flex",top:"80%",left:"80%"}}><FontAwesomeIcon icon={faMicrochip}/></p>
+          <p data-speed="2" className="layer" style={{display:"flex",top:"84%",left:"30%"}}><FontAwesomeIcon icon={faSatellite}/></p>
         </section>
 
         <section id="about" className={styles.about}>
           <h1>{t("about:title")}</h1>
-          <section>
-            <div>
-            <span><h2>{t("about:name")}</h2></span>
+          <div>
+            <section>
+              <h2><span>{t("about:name")}</span></h2>
               <span>
                 <p><FontAwesomeIcon icon={faBirthdayCake}/> {t("about:dateOfBirth")}</p>
                 <h5>20 March 2003</h5>
@@ -91,9 +104,6 @@ export default function Home({ projects }) {
                 <p><FontAwesomeIcon icon={faEnvelope}/> {t("about:email")}</p>
                 <h5>Papop2003@gmail.com</h5>
               </span>
-            </div>
-            <div>
-            <span><h2>‎</h2></span>
               <span>
                 <p><FontAwesomeIcon icon={faPhone}/> {t("about:phone")}</p>
                 <h5>+66 89 811 8068</h5>
@@ -106,9 +116,10 @@ export default function Home({ projects }) {
                 <p><FontAwesomeIcon icon={faGraduationCap}/> {t("about:education")}</p>
                 <h5>{t("about:school")}</h5>
               </span>
-            </div>
-            <div>
-            <span><h2>{t("about:organisation")}</h2></span>
+            </section>
+            
+            <section>
+              <span><h2>{t("about:organisation")}</h2></span>
               <span>
                 <p><FontAwesomeIcon icon={faUserAstronaut}/> SPACE AC</p>
                 <h5>{t("about:spaceRole")}</h5>
@@ -121,23 +132,21 @@ export default function Home({ projects }) {
                 <p><FontAwesomeIcon icon={faGraduationCap}/> GenSTEM</p>
                 <h5>{t("about:genstemRole")}</h5>
               </span>
-            </div>
-            <div>
-            <span><h2>{t("about:skill")}</h2></span>
-              <span >
+              <span><h2>{t("about:skill")}</h2></span>
+              <span>
                 <p><FontAwesomeIcon icon={faCode}/> {t("about:programming")}</p>
                 <h5>Python, C, C++, Javascript, HTML, CSS</h5>
-                </span>
-                <span >
-                  <p><FontAwesomeIcon icon={faLanguage}/> {t("about:language")}</p>
-                  <h5>{t("about:spokenLanguage")}</h5>
-                </span>
-                <span className={styles.aboutcard}>
-                  <p><FontAwesomeIcon icon={faCamera}/> {t("about:other")}</p>
-                  <h5>{t("about:otherSkill")}</h5>
               </span>
-            </div>
-          </section>
+              <span>
+                <p><FontAwesomeIcon icon={faLanguage}/> {t("about:language")}</p>
+                <h5>{t("about:spokenLanguage")}</h5>
+              </span>
+              <span>
+                <p><FontAwesomeIcon icon={faCamera}/> {t("about:other")}</p>
+                <h5>{t("about:otherSkill")}</h5>
+              </span>
+            </section>
+          </div>
           <AwesomeButton className="Button" size="large" type="primary" onPress={() => router.push("/about")}><h3>More</h3></AwesomeButton>
         </section >
         <section className={styles.projects} id="projects">
